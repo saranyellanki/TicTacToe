b04b386338ef52a6d3c13eadd6daa5e7d4570568
saranyellanki <saran.yellanki12@gmail.com>
2021-10-21 23:33:54 +0530

    create game board

--- src/main/java/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/main/java/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Arrays;
+
 public class TicTacToe {
+    public char[] board;
+    /*
+        constructor for TicTacToe which initializes
+        character array board with length 10 as default
+     */
+    public TicTacToe() {
+        this.board = new char[10];
+    }
+
+    public void createGameBoard() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        System.out.println(Arrays.toString(board));
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to TicTcToe Game");
+        TicTacToe ticTacToeObj = new TicTacToe();
+        ticTacToeObj.createGameBoard();
     }
 }
